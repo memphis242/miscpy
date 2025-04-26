0ea345465336a608928aa32bd7fec16e5828724c
memphis242 <abdullah.almosalami.eng@gmail.com>
2025-04-25 20:32:36 -0500

    :building_construction: Account for indentation

--- centered_text_with_border.py
+++ centered_text_with_border.py
@@ -20,7 +20,7 @@ def print_centered_text_with_border(text, border_char, width=80):
         raise ValueError("Error: <border_char> must be a single character.")
     if not border_char.isprintable():
         raise ValueError("Error: <border_char> must be a printable character.")
-    if width <= len(text) + 2:
+    if width <= (len(text) + 2):
         raise ValueError("Error: Width must be greater than the length of the text plus borders.")
 
     text_length = len(text) + 2  # Account for spaces around the text
@@ -37,7 +37,7 @@ def print_centered_text_with_border(text, border_char, width=80):
     print("The centered text has been copied to your clipboard.")
 
 
-def print_c_comment(text, width=80):
+def print_c_comment(text, width=80, indent=0):
     """
     Prints a C comment with text centered in the form below:
 
@@ -54,13 +54,13 @@ def print_c_comment(text, width=80):
     Raises:
         ValueError: If the text is too long.
     """
-    if width <= (len(text) + 2):
-        raise ValueError("Error: Width must be greater than the length of the text plus borders.")
+    if width <= (len(text) + 2 + 2 + indent):   # 2 for spaces, 2 for '/' characters
+        raise ValueError(f"Error: Width is too short. Needs to at least be {len(text) + 2 + 2 + indent}.")
 
-    text_length = len(text) + 2  # Account for spaces around the text
+    text_length = len(text) + 2 + indent  # Account for spaces around the text
     star_border_length = math.floor((width - text_length) / 2) - 2 # Take 2 off for the two '/' characters
     star_border = '*' * star_border_length
-    centered_text = f"/{star_border} {text} {star_border}/"
+    centered_text = (' ' * indent) + f"/{star_border} {text} {star_border}/"
 
     print(centered_text)
     pyperclip.copy(centered_text)  # Copy the centered text to the clipboard
@@ -74,16 +74,22 @@ if __name__ == "__main__":
     flag = sys.argv[1]
 
     if flag == '-c':
-        if len(sys.argv) == 4:
+
+        if len(sys.argv) >= 4:
             try:
                 width = int(sys.argv[3])
-                print_c_comment( sys.argv[2], width )
             except ValueError:
                 print("Error: Width must be an integer.")
                 sys.exit(1)
-        
-        else:
-            print_c_comment( sys.argv[2] )
+
+        if len(sys.argv) >= 5:
+            try:
+                width = int(sys.argv[4])
+            except ValueError:
+                print("Error: Indentation must be an integer.")
+                sys.exit(1)
+
+        print_c_comment( sys.argv[2], width )
 
     else:
         user_input = sys.argv[1]
